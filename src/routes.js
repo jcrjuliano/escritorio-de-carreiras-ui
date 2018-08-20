c1f029ee60d088246be1b632758c21056c1ebbf2
Mario Taddeucci <taddeucci23@gmail.com>
2018-08-20 20:20:00 -0300

    CRUD INSTITUTION + JOBS
    Correção de Instituion para Institution
    CRUD Completo sem testes Institution
    Delete em JOBS

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import Home from './router/Home.vue';
 import P404 from './router/404.vue';
+import P403 from './router/403.vue';
 import Jobs from './router/Jobs.vue';
-import Instituion from './router/Instituion.vue';
+import Institutions from './router/Institutions.vue';
+
+import Institution from './router/components/Institution.vue';
 var menu_tree = [
 {path:'/', name:'home', component:Home},
 /*
@@ -13,8 +16,9 @@ var menu_tree = [
 {path:'/licitacoes/:page', name:'licitacoes', component:Licitacoes},
 {path: '/licitacoes', redirect: {name:'licitacoes', params:{page:1}}},
 */
-{path: '/instituion', name:'instituion', component: Instituion },
+{path: '/institution/:id', name:'institution', component: Institution, props: true  },
 {path: '/jobs', name:'jobs', component: Jobs },
+{path: '/403', name:'403', component: P403 },
 {path: '/404', name:'404', component: P404 },
 {path: '*', redirect: {name:'404'}},
 ]
